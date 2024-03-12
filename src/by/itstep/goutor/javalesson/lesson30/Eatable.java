31b81e695b719e24263ef1971b7cb56041e65a8c
nikishkaa <nikita.goutor@gmail.com>
2024-03-12 21:39:07 +0300

    Interface.

---
+++ src/by/itstep/goutor/javalesson/lesson30/Eatable.java
@@ -0,0 +1,24 @@
+package by.itstep.goutor.javalesson.lesson30;
+
+public interface Eatable {
+    int SIZE = 10;
+
+    void eat();
+
+    // Since JDK 8.0
+    default int sum(int a, int b) {
+        return a + b;
+    }
+
+    // Since JDK 8.0
+    public static void main(String[] args) {
+
+    }
+
+    // Since JDK 9.0
+    // private static final int COUNT = 10;
+
+    private void test() {
+
+    }
+}
---
+++ src/by/itstep/goutor/javalesson/lesson30/Main.java
@@ -0,0 +1,12 @@
+package by.itstep.goutor.javalesson.lesson30;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void testing(Eatable eatable) {
+        eatable.eat();
+    }
+}
